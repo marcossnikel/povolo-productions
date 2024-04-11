71a94038c16607c0f0c4a59db808fbb084a2241d
marcossnikel <marcosnikeldev@gmail.com>
2024-04-11 14:23:51 -0300

    feat: Add navbar inicial no layout da pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Services } from "@/components/navigation-services";
+import Link from "next/link";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: "500", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Povolo Produções",
@@ -16,7 +19,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${poppins.className} bg-zinc-100`}>
+        <nav className="flex justify-around items-center fixed z-10 w-full bg-slate-600">
+          <div className="flex gap-1">
+            <Image src="/logo.png" width={50} height={50} alt="" />
+            <div className="flex flex-col"></div>
+          </div>
+          <div className="flex gap-6 items-center text-xl">
+            <Link href="/" className="hover:underline underline-offset-8 ">
+              Home
+            </Link>
+            <Link href="/about" className="hover:underline underline-offset-8 ">
+              Sobre
+            </Link>
+            <Services />
+            <a
+              href="#depoimentos"
+              className="hover:underline underline-offset-8 "
+            >
+              Depoimentos
+            </a>
+            <a href="" className="hover:underline underline-offset-8 ">
+              Contato
+            </a>
+          </div>
+        </nav>
+        {children}
+        <footer>
+          <p className="text-center">
+            2024 &#169; Todos os direitos reservados a Povolo Produçoes
+          </p>
+        </footer>
+      </body>
     </html>
   );
 }
