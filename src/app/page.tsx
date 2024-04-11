42b6fdbbfde95a415f789484750183244a92d7be
marcossnikel <marcosnikeldev@gmail.com>
2024-04-11 15:14:00 -0300

    testing the ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-2 p-24"></main>
+    <main className="flex min-h-screen flex-col items-center gap-2 p-24">
+      hello world!
+    </main>
   );
 }
